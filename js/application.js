1455eccebcf10537528d4eb106608ac9fa07ae75
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2013-08-29 15:52:04 +0200

    Work

--- js/application.js
+++ js/application.js
@@ -2,11 +2,22 @@
 
   $(function(){
     $('body').scrollspy({ target: '.main-nav-container', offset: 200 });
-    /*
-    $('.section').on('activate.bs.scrollspy', function () {
+    
+    $("body").on('activate.bs.scrollspy', function (e) {
+        // Also set the mobile menu
+        /*
+        var href = $(e.target).find("a").attr("href");
+        var mobile_item_selector = '.mobile-nav .navbar-nav a[href="' + href + '"]';
+        //console.log($(mobile_item_selector));
+        //console.log($(mobile_item_selector).parent());
+        $(mobile_item_selector).parents('.active').removeClass('active');
         
-    })
-    */
+
+        $(mobile_item_selector).parent().addClass("active");
+        console.log($(mobile_item_selector).parents('.active'));
+        */
+    });
+
     
 });
 
