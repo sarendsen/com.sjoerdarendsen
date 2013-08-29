81f7870ce1f04b54c4014994deca7d1348ba1162
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2013-08-29 20:40:57 +0200

    lala

--- js/application.js
+++ js/application.js
@@ -1,24 +1,15 @@
 !function ($) {
 
   $(function(){
-    $('body').scrollspy({ target: '.main-nav-container', offset: 200 });
-    
+    $('body').scrollspy({ target: '.navbar', offset: 100 });
     $("body").on('activate.bs.scrollspy', function (e) {
-        // Also set the mobile menu
-        /*
+        // Also set the desktop menu
         var href = $(e.target).find("a").attr("href");
-        var mobile_item_selector = '.mobile-nav .navbar-nav a[href="' + href + '"]';
-        //console.log($(mobile_item_selector));
-        //console.log($(mobile_item_selector).parent());
-        $(mobile_item_selector).parents('.active').removeClass('active');
-        
-
+        var mobile_item_selector = '.main-nav a[href="' + href + '"]';
+        $(".main-nav li.active").removeClass('active');
         $(mobile_item_selector).parent().addClass("active");
-        console.log($(mobile_item_selector).parents('.active'));
-        */
     });
 
-    
 });
 
 }(window.jQuery);
